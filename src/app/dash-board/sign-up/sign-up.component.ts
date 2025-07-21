c9e2c6e874447dafb9d3277e60a02cf573700ea7
rudra_pratap_sahoo <rudrasahoo848@gmail.com>
2025-07-21 11:10:54 +0530

    User Sing-Up component

--- src/app/dash-board/sign-up/sign-up.component.ts
+++ src/app/dash-board/sign-up/sign-up.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {}
+export class SignUpComponent implements OnInit {
+  // injects
+  private fb = inject(FormBuilder);
+  private api = inject(ApiService);
+  private route = inject(Router);
+
+  // form
+  SignupFrom!: FormGroup;
+
+  ngOnInit(): void {
+    this.SignupFrom = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  /** Functions */
+  // Submit
+  onSubmit() {
+    if (this.SignupFrom.invalid) {
+      this.SignupFrom.markAllAsTouched();
+      return;
+    }
+    const Payload = this.SignupFrom.value;
+    this.api.UserSignup(Payload).subscribe({
+      next: (res) => {
+        console.log(res);
+        alert(' SignUp Sucessfull');
+        this.route.navigate(['/DashBoard/SignIn']);
+      },
+      error: (err) => {
+        alert('SignUp Failed');
+      },
+    });
+  }
+}
